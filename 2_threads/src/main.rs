6e254032bbca9e10aba78e8c34c1a1b61b0bd85a
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-16 13:35:45 +0000

    thread pool not quite working

--- 2_threads/src/main.rs
+++ 2_threads/src/main.rs
@@ -1,15 +1,37 @@
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
+use std::sync::{mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-struct ThreadPool {}
+struct ThreadPool {
+    handles: Vec<thread::JoinHandle<()>>,
+    sender: Sender<Box<dyn FnOnce() + 'static + Send>>,
+}
 
 impl ThreadPool {
     fn new(number_of_threads: u8) -> Self {
-        todo!()
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut handles = Vec::with_capacity(number_of_threads.into());
+
+        for n in 0..number_of_threads {
+            let receiver_clone = receiver.clone();
+            handles.push(thread::spawn(move || {
+                let task = receiver_clone.lock().unwrap().recv().unwrap();
+            }))
+        }
+
+        Self { handles, sender }
     }
 
-    fn execute(&self, task: ?) {
-        todo!()
+    fn execute<F>(&self, task: F)
+    where
+        F: FnOnce() + 'static + Send,
+    {
+        self.sender.send(Box::new(task)).unwrap()
     }
 }
 
