e91de61c53f2c168528addd4c0743eaa6b24afa7
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-16 16:04:25 +0000

    extract out todo list component

--- 4_web-assembly/src/app.rs
+++ 4_web-assembly/src/app.rs
@@ -1,3 +1,4 @@
+use crate::components::todoList::TodoList;
 use yew::prelude::*;
 
 #[function_component(App)]
@@ -12,32 +13,7 @@ pub fn app() -> Html {
                 <section class="main">
                     <input id="toggle-all" class="toggle-all" type="checkbox" />
                     <label for="toggle-all" />
-                    <ul class="todo-list">
-                        <li>
-                            <div class="view">
-                                <input type="checkbox" class="toggle" />
-                                <label>{"TODO 1"}</label>
-                                <button class="destroy" />
-                            </div>
-                            <input class="edit" type="text" value={"TODO 1"} hidden={true} />
-                        </li>
-                        <li class={"completed"}>
-                            <div class="view">
-                                <input type="checkbox" class="toggle" checked={true} />
-                                <label>{"TODO 2"}</label>
-                                <button class="destroy" />
-                            </div>
-                            <input class="edit" type="text" value={"TODO 2"} hidden={true} />
-                        </li>
-                        <li class={"editing"}>
-                            <div class="view">
-                                <input type="checkbox" class="toggle" />
-                                <label>{"TODO 3"}</label>
-                                <button class="destroy" />
-                            </div>
-                            <input class="edit" type="text" value={"TODO 3"} />
-                        </li>
-                    </ul>
+                    <TodoList />
                 </section>
                 <footer class="footer">
                     <span class="todo-count">
