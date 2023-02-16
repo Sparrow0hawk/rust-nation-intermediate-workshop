ae4a40ae70ef5f875a51c5b9426d1b71ef8057a1
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-16 16:18:09 +0000

    add component logic for presenting todos

--- 4_web-assembly/src/components/todoList.rs
+++ 4_web-assembly/src/components/todoList.rs
@@ -1,33 +1,65 @@
-use yew::{function_component, html, Html};
+use yew::prelude::*;
+
+#[derive(PartialEq)]
+enum Mode {
+    New,
+    Complete,
+    Editing,
+}
+
+#[derive(PartialEq)]
+struct Todo {
+    label: String,
+    mode: Mode,
+}
+
+impl Todo {
+    fn new(label: &str, mode: Mode) -> Self {
+        Self {
+            label: label.to_string(),
+            mode,
+        }
+    }
+}
 
 #[function_component(TodoList)]
 pub fn todo_list() -> Html {
+    let todo = Todo::new("TODO 2b", Mode::New);
+    let todo2 = Todo::new("TODO 2c", Mode::Complete);
+    let todo3 = Todo::new("TODO 100---01", Mode::Editing);
+
     html! (
         <ul class="todo-list">
-        <li>
-            <div class="view">
-                <input type="checkbox" class="toggle" />
-                <label>{"TODO 1"}</label>
-                <button class="destroy" />
-            </div>
-            <input class="edit" type="text" value={"TODO 1"} hidden={true} />
-        </li>
-        <li class={"completed"}>
-            <div class="view">
-                <input type="checkbox" class="toggle" checked={true} />
-                <label>{"TODO 2"}</label>
-                <button class="destroy" />
-            </div>
-            <input class="edit" type="text" value={"TODO 2"} hidden={true} />
-        </li>
-        <li class={"editing"}>
-            <div class="view">
-                <input type="checkbox" class="toggle" />
-                <label>{"TODO 3"}</label>
-                <button class="destroy" />
-            </div>
-            <input class="edit" type="text" value={"TODO 3"} />
-        </li>
+        <TodoItem todo={todo}/>
+        <TodoItem todo={todo2}/>
+        <TodoItem todo={todo3}/>
     </ul>
+    )
+}
+
+#[derive(Properties, PartialEq)]
+struct TodoItemProps {
+    todo: Todo,
+}
+
+#[function_component(TodoItem)]
+fn todo_item(props: &TodoItemProps) -> Html {
+    let class_mode = match props.todo.mode {
+        Mode::New => None,
+        Mode::Complete => Some("Completed"),
+        Mode::Editing => Some("editing"),
+    };
+
+    html! (
+    <li class={classes!(class_mode)}>
+        <div class="view">
+            <input type="checkbox" class="toggle"
+                checked={matches!(props.todo.mode, Mode::Complete)}/>
+            <label>{&props.todo.label}</label>
+            <button class="destroy" />
+        </div>
+        <input class="edit" type="text" value={props.todo.label.clone()}
+            hidden={!matches!(props.todo.mode, Mode::Editing)}/>
+    </li>
     )
 }
